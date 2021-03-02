3f0391eb9b9e7d24bd12608757b1c89c53f94b81
memoTux <romeo@mendezfuentes.net>
2021-03-02 15:33:43 -0600

    Fix: Gatsby Plugin PostCSS compatibility.

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -8,6 +8,7 @@ const Row: React.FC<IRow> = ({ cols, children }) => {
   return (
     <div
       className={`grid grid-cols-${cols} justify-items-center place-items-center w-full p-4 space-x-4`}
+      style={{ gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }}
     >
       {children}
     </div>
