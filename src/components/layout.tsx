cba11caad8514d7c2aa5c6854beedcfdc9ab2688
memoTux <romeo@mendezfuentes.net>
2021-03-04 11:46:20 -0600

    Update: readme & layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,6 +28,24 @@ const Layout: React.FC = ({ children }) => {
       <div className="mx-auto max-w-7xl p-4">
         <main>{children}</main>
         <footer className="mt-8">
+          <p className="text-xs">
+            Presentamos al público general nuestras consideraciones ante los
+            datos publicados tanto por el
+            <a href="https://elecciones2021.tse.gob.sv">TSE</a> como los medios
+            de comunicación nacionales. Al hacer uso de las publiaciones de
+            resultados preliminares, especialmente para Asamblea Legislativa,
+            hemos encontrado: 1. Que se han utlizado los datos a nivel nacional
+            de la publicacion: #1097 - 01/03/2021 08:34:00 PM 2. Que al hacer un
+            calculo de Cociente y Residuos sobre el dato nacional, la cantidad
+            de diputados electos difiere del total de diputados electos por
+            departamento. 3. Los diferentes medios de comunicación han
+            publicado, llevando a un error a la población electora, que N tiene
+            56 diputados y ARENA 14 diputados. 4. Cuando se hace la sumatoria de
+            diputados electos por departamento, esta reflea que N tiene 60
+            diputados y ARENA 10 diputados. Todo esto ha hecho caer en un error
+            a la poblacion electora y por lo tanto nuestro único interés es
+            informarla debidamente.
+          </p>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
