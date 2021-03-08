6631b325ab410297fd9e3650866ba365247b526f
memoTux <romeo@mendezfuentes.net>
2021-03-08 00:29:08 -0600

    Update: Header add nav.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,23 @@ import { Link } from 'gatsby'
 import React from 'react'
 
 const Header = ({ siteTitle }: { siteTitle: string }) => (
-  <header className="bg-indigo-900 mb-7">
+  <header className="bg-blue-900 mb-7 grid grid-cols-2 justify-items-around place-items-center">
     <div className="mx-auto max-w-7xl p-6">
       <h1 className="m-0">
-        <Link to="/" className="text-white no-underline">
+        <Link to="/" className="no-underline">
           {siteTitle}
         </Link>
       </h1>
     </div>
+    <nav className="grid grid-cols-2 place-items-center place-content-around">
+      <Link to="/" activeClassName="active-link">
+        Final
+      </Link>
+      <Link to="/escrutinio-preliminar" activeClassName="active-link">
+        Preliminar
+      </Link>
+      {/* <Link to="#">Inconsistencias</Link> */}
+    </nav>
   </header>
 )
 
