7a966b0e6dcd1ef44325531a245d6b39627808d4
memoTux <romeo@mendezfuentes.net>
2021-03-08 00:29:22 -0600

    New: Selector component

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 
-export default Selector: React.FC<{options: {value: any, text: string}[] | string[]}> ({options, ...selectProps}) => {
-    return (
-        <select {...selectProps}>
-            {options.map(
-                ({value, text}) => {
-                    return (
-                        <option value={value}>{text}</option>
-                    )
-                }
-            )}
-        </select>
-    )
+const Selector: React.FC<
+  React.DetailedHTMLProps<
+    React.SelectHTMLAttributes<HTMLSelectElement>,
+    HTMLSelectElement
+  > & {
+    options: { value: any; text: string }[] | string[]
+  }
+> = ({ options, ...selectProps }) => {
+  return (
+    <select {...selectProps}>
+      {options.map((option: any) => {
+        if (typeof option === 'string') {
+          return (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          )
+        }
+        const { value, text } = option
+        return (
+          <option key={value} value={value}>
+            {text}
+          </option>
+        )
+      })}
+    </select>
+  )
 }
+
+export default Selector
