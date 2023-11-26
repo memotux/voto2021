0cce77074fbc99a2f5fb528ee094a30356c82499
memoTux <romeo@mendezfuentes.net>
2023-11-26 11:31:10 -0600

    update global.css

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -3,7 +3,7 @@
     class="dark:bg-blue-900 mb-7 grid grid-cols-2 justify-items-around place-items-center"
   >
     <div class="mx-auto p-6">
-      <h1 class="m-0">
+      <h1 class="m-0 font-normal">
         <NuxtLink aria-current="page" class="no-underline" href="/"
           >Voto 2021 | Asamblea Legislativa</NuxtLink
         >
