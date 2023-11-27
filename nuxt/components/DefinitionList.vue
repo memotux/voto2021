637ae8be1f3030e8e20ccccb265ed12a4e558277
memoTux <romeo@mendezfuentes.net>
2023-11-27 10:09:01 -0600

    add component DefinitionList

---
+++ nuxt/components/DefinitionList.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+  items: Array<[string, string]>
+}>()
+</script>
+
+<template>
+  <dl class="divide-y-2 divide-primary-50/25">
+    <div v-for="item in items" class="flex justify-between items-center p-1">
+      <dt class="w-1/2">{{ item[0] }}</dt>
+      <dd class="w-1/2 text-right">
+        {{ item[1] }}
+      </dd>
+    </div>
+  </dl>
+</template>
