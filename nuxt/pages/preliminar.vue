516ee78af74865677adbe913f055f4f358699c82
memoTux <romeo@mendezfuentes.net>
2023-12-06 10:58:56 -0600

    pages/preliminar message

--- nuxt/pages/preliminar.vue
+++ nuxt/pages/preliminar.vue
@@ -1,7 +1,6 @@
-<script lang="ts" setup></script>
-
 <template>
   <div>
-    Page: Preliminar
+    <h1>Lo sentimos!</h1>
+    <h2>Esta información ya no está disponible.</h2>
   </div>
 </template>
